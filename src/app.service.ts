6190fee6c9231d511b714e94e291f09922db696a
Wiliam <ariafardhossein@gmail.com>
2025-12-02 13:48:00 +0330

    improved text.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello guys welcome to my first of back-end project!';
   }
 }
